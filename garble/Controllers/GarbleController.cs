f62d744a5406173c177fdc602e5b2f720a88e445
mikedowd <mdowd@nexxofinancial.com>
2016-04-07 21:14:30 -0700

    creating the restful garbleizer web svc

---
+++ garble/Controllers/GarbleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace garble.Controllers
+{
+    [Route("api/[controller]")]
+    public class GarbleController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{word}")]
+        public string Get(string word)
+        {
+			return string.Format( $"oiuwq{word}erkjs" );
+        }
+    }
+}
